86d1a03360c821136ec1771a113d17a07eccefea
Pedro Renã <pedrorenanmoreira@gmail.com>
2025-10-02 18:09:33 -0300

    iniciando db, atualizando name para nome

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from util.database import init_db
+from controller.pessoa import router as pessoas_router
+from controller.endereco import endereco_router as enderecos_router
 
 app = FastAPI()
+
+init_db()
+
+app.include_router(pessoas_router)
+app.include_router(enderecos_router)
 
 @app.get('/')
 def health():
--- model/models.py
+++ model/models.py
@@ -1,23 +1,20 @@
-from sqlmodel import Field, SQLModel, Relationship
-from typing import Optional, List
+from sqlmodel import Field, SQLModel
+from typing import Optional
 
 class PessoaBase(SQLModel):
-    name: str = Field(min_length=2, max_length=120)
+    nome: str = Field(min_length=2, max_length=120)
     idade: Optional[int] = Field(default=None, ge=0, le=200)
     email: Optional[str] = Field(max_length=120)
 
 class Pessoa(PessoaBase, table=True):
-    __tablename__ = Pessoa
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    enderecos: List["Endereco"] = Relationship(back_populates="pessoa")
-
-
-class Endereco(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    
+class EnderecoBase(SQLModel):
     logradouro: Optional[str] = Field(max_length=120)
     numero: Optional[int] = Field(default=None)
     estado: Optional[str] = Field(max_length=2, min_length=2)
     cidade: Optional[str] = Field(max_length=120)
     bairro: Optional[str] = Field(max_length=120)
-    pessoa_id: Optional[int] = Field(default=None, foreign_key="pessoa.id")
-    pessoa: Optional["Pessoa"] = Relationship(back_populates="enderecos")
+
+class Endereco(EnderecoBase, table= True):
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
