664c8341583df2c6e1605a8ba667933458ec39ba
Pedro Renã <pedrorenanmoreira@gmail.com>
2025-11-06 21:29:34 -0300

    adaptando requirements e organizando pastas

--- model_pessoasenderecos_lib/setup.py
+++ model_pessoasenderecos_lib/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="model-pessoasenderecos-lib",  # ⚡ Nome com hífen para o PyPI
-    version="0.1.1",
+    version="0.1.2",
     description="Lib model de pessoas e endereços.",
     long_description=long_description,
     long_description_content_type="text/markdown",
